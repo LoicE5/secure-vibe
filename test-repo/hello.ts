c4d21cb3b8a0e812aea5d5f138471a778338c3bb
Loïc <59501884+LoicE5@users.noreply.github.com>
2026-03-07 02:02:20 +0100

    chore: remove hello.ts example file

--- test-repo/hello.ts
+++
@@ -1,4 +0,0 @@
-// Yes this isn't much but it's honest work
-// You can run this file in the container using the built-in bun
-// Need more ? Built-in brew is here to get you started with node, deno or anything you'd like really
-console.info("Hello world!")
